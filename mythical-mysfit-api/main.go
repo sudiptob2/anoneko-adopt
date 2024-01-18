5f6937f2c29ef404bd23bed16727cd29e5e9512c
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-18 16:35:17 -0500

    setup: setup dev env with docker-compose
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- mythical-mysfit-api/main.go
+++ mythical-mysfit-api/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	r := gin.Default()

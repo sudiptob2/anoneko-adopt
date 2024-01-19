6e8ada473be6d3a1c74e25979c8352b2966ec047
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-19 18:15:10 -0500

    feat: env variable utilized
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- anoneko-adopt-front/src/routes/+page.svelte
+++ anoneko-adopt-front/src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 	export let data;
 	console.log(data);
-	let showFullDescription = false;
-	function toggleDescription() {
-		showFullDescription = !showFullDescription;
-	}
 </script>
 
 <div class="grid grid-cols-1 text-center mt-3 mb-6 font-light">
@@ -15,30 +11,32 @@
 </div>
 
 <div class="mx-10 sm:mx-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-	{#each data.contents as pet}
-		<div class="card glass">
-			<figure class="h-[150px]">
-				<img class="h-[150px] object-cover" src={pet.thumbImageUri} alt="pet!" />
-			</figure>
-			<div class="card-body">
-				<div>
-					<h2 class="card-title">{pet.name}</h2>
-					<div class="badge badge-neutral">{pet.lawchaos}</div>
-					<div class="badge badge-accent">{pet.goodevil}</div>
-					<div class="badge badge-info">age: {pet.age}</div>
-				</div>
+	{#if data?.contents}
+		{#each data?.contents as pet}
+			<div class="card glass">
+				<figure class="h-[150px]">
+					<img class="h-[150px] object-cover" src={pet.thumbImageUri} alt="pet!" />
+				</figure>
+				<div class="card-body">
+					<div>
+						<h2 class="card-title">{pet.name}</h2>
+						<div class="badge badge-neutral">{pet.lawchaos}</div>
+						<div class="badge badge-accent">{pet.goodevil}</div>
+						<div class="badge badge-info">age: {pet.age}</div>
+					</div>
 
-				<div class="text-justify">
-					<p>
-						{pet.description.substring(0, 150)}...
-					</p>
-				</div>
+					<div class="text-justify">
+						<p>
+							{pet.description.substring(0, 150)}...
+						</p>
+					</div>
 
-				<div class="card-actions justify-center">
-					<button class="btn btn-primary">Adopt</button>
-					<button class="btn btn-secondary ml-2">View Profile</button>
+					<div class="card-actions justify-center">
+						<button class="btn btn-primary">Adopt</button>
+						<button class="btn btn-secondary ml-2">View Profile</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	{/each}
+		{/each}
+	{/if}
 </div>

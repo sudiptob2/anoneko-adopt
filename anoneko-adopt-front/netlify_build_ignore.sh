cdc97875edc69cf2e1c6aeaf0bf0da2e96933c1f
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-21 15:11:01 -0500

    ci: test netlify [#33]
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- anoneko-adopt-front/netlify_build_ignore.sh
+++ anoneko-adopt-front/netlify_build_ignore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if git diff --quiet HEAD^ HEAD anoneko-adopt-front
+if git diff HEAD^ HEAD
 then
   # No changes in anoneko-adopt-front, exit with 0 to skip build
   echo "No changes in anoneko-adopt-front, skipping build"
